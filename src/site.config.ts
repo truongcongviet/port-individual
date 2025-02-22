0d61427dc67f829e80f22c611572900d3a5c5cb1
viettc <145972097+viettc98@users.noreply.github.com>
2025-02-23 00:37:06 +0700

    config author

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
+	author: 'VietTruong',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'adlonymous',
+	title: 'Viet Truong Blog',
 	// Meta property used as the default description meta property
-	description: "Aadil 's personal website + blog ",
+	description: "Viet Truong 's personal website + blog ",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
